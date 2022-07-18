1a04bbc2893b0a0422bda3fb55fdf2c630e8a894
tcarlucc <tcarlucc@uvm.edu>
2022-07-17 21:31:50 -0400

    basic text commands

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import discord
+import os
+import secrets  # Seperate python file to not expose private token for bot login
 
 client = discord.Client()
 
+
 @client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$ping'):
+        await message.channel.send('pong!')
+
+    if message.content.startswith('$play flexicution'):
+        await message.channel.send('no!')
+
+
+client.run(secrets.token)
