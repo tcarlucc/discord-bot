4333e0b95e0e791f2d422e34b28bbc1d9041061a
tcarlucc <tcarlucc@uvm.edu>
2022-07-18 20:35:40 -0400

    Added some basic config variables to store reused global variables (like error messages, headers, etc.)

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
 PREFIX = '$'
 
 TIMEOUT = 600  # seconds
+
+# HEADERS
+
+HISTORY_HEADER = "Song History:"
+
+# ERROR MESSAGES
+
+USER_NOT_IN_VOICE = "You are not in a voice chat. Please join one to use this command."
