8f2703f4486e655ae6f9856fde4a02c176c15a5c
tcarlucc <tcarlucc@uvm.edu>
2022-07-19 23:24:56 -0400

    Added get_url to linkprocessor. Checks if valid link using regex

--- linkprocessor.py
+++ linkprocessor.py
@@ -2,3 +2,12 @@ import re
 
 url_regex = re.compile("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
 
+def get_url(msg):
+    regex = re.compile("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
+
+    if re.search(regex, msg):
+        result = regex.search(msg)
+        url = result.group(0)
+        return url
+    else:
+        return None
