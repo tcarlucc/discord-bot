cf0e7bc3c2d16091b1969a9a0cb530f334c3f7d5
tcarlucc <tcarlucc@uvm.edu>
2022-07-19 23:28:00 -0400

    Added enum for known sites. So far just youtube, plam to add spotify later on. Also added identify_url function

--- linkprocessor.py
+++ linkprocessor.py
@@ -1,5 +1,11 @@
 import re
 
+from enum import Enum
+
+class Sites(Enum):
+    YouTube = "YouTube"
+    Unknown = "Unknown"
+
 url_regex = re.compile("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
 
 def get_url(msg):
@@ -11,3 +17,12 @@ def get_url(msg):
         return url
     else:
         return None
+
+
+def identify_url(msg):
+    if msg is None:
+        return Sites.Unknown
+    elif "https://www.youtu" in msg or "https://youtu.be" in msg:
+        return Sites.YouTube
+    else:
+        return Sites.Unknown
