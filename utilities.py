ab55dc95d048acb55272d712d8e9ce59f542bd79
tcarlucc <tcarlucc@uvm.edu>
2022-07-19 23:05:41 -0400

    Added get_guild to utilities.py

--- utilities.py
+++ utilities.py
@@ -6,3 +6,12 @@ async def is_connected(ctx):
         return voice_channel
     except:
         return None
+
+def get_guild(bot, command):
+    if command.guild is not None:
+        return command.guild
+    for guild in bot.guilds:
+        for channel in guild.voice_channels:
+            if command.author in channel.members:
+                return guild
+    return None
